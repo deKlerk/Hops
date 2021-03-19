de13960f72b52f3524d8b83479a9500c74e2411b
ruideklerk <rvzk.mota@gmail.com>
2021-03-19 23:34:34 +0000

    first commit -> app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import ghhops_server as hs
+from ghhops_server.params import HopsCurve, HopsNumber
+import rhino3dm as rh
+
+# register hops app as middleware
+app = Flask(__name__)
+hops = hs.Hops(app)
+
+
+#-- Point At component --#
+@hops.component(
+    "/pointat",
+    name="PointAt",
+    description="Get a point along a curve",
+    inputs=[
+        hs.HopsCurve("Curve", "C", "Curve to evaluate"),
+        hs.HopsNumber("t", "t", "Parameter on Curve to evaluate.")
+    ],
+    outputs=[
+        hs.HopsPoint("P", "P", "Point on Curve at parameter t")
+    ]
+)
+def pointat(curve: rh.Curve, t: float=0):
+    pt = curve.PointAt(t)
+    return pt
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
