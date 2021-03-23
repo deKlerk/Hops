516a2c04e66b5a8bd7ac92eb9df8126a41d50b50
ruideklerk <rvzk.mota@gmail.com>
2021-03-23 23:23:18 +0000

    added endpoints: plevels and outlists

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import ghhops_server as hs
-from ghhops_server.params import HopsCurve, HopsNumber
+from ghhops_server.params import HopsCurve, HopsInteger, HopsNumber
 import rhino3dm as rh
 
 
@@ -30,6 +30,47 @@ def pointat(curve: rh.Curve, t: float=0):
     pt = curve.PointAt(t)
     return pt
 
+
+#-- Profile Levels component --#
+@hops.component(
+    "/plevels",
+    name="ProfileLevels",
+    description="Creates a series of levels from a profile",
+    inputs=[
+        hs.HopsCurve("Curve", "C", "Curve to replicate"),
+        hs.HopsNumber("D", "D", "Distance between levels"),
+        hs.HopsInteger("N", "N", "Number of levels")
+    ],
+    outputs=[
+        hs.HopsCurve("Curves", "Cs", "Series of profile curves")
+    ]
+)
+def plevels(curve: rh.Curve, d: float=1.0, n: int=1):
+    crvs = [curve]
+    temp = 0.0
+
+    for i in range(n+1):
+        crv = curve
+        temp += d
+        vec = rh.Vector3d(0,0,temp)
+        trans = rh.Transform.Translation(vec)
+        crv.Transform(trans)
+        crvs.append(crv)
+
+    return crvs
+
+#-- Test Lists component --#
+@hops.component(
+    "/outlists",
+    name="OutLists",
+    nickname="OutLists",
+    description="Trying list output",
+    inputs=[hs.HopsInteger("N", "N", "Number of items in list")],
+    outputs=[hs.HopsInteger("out", "out", "list of numbers")]
+)
+def outlist(n: int=1):
+    lst = [i for i in range(n)]
+    return lst
 
 if __name__ == '__main__':
     # set debug=True
